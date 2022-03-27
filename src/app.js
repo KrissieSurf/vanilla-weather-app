ddaf36530371cdf70b04a999ada1bbd144767ede
KrissieSurf <krissie@kollectify.com>
2022-03-27 14:41:24 +0100

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -9,27 +9,29 @@ function formatDate(timestamp) {
         minutes = `0${minutes}`;
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
-   
-    }
+    return `${day} ${hours}:${minutes}`; }
 }
 function displayTemperature(response) {
-    console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
+
 temperatureElement.innerHTML = Math.round (response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "a6d661b9ded656b75eb572b4dff7051b";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=New York&&appid=${apiKey}&units=metric`;
+let city = "Lisbon";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log (apiURL);
 axios.get(apiURL).then(displayTemperature);
